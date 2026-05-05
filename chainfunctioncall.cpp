28f91aecde48889844bc76376bdecfeb25413b4f
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-05 20:19:57 +0700

    add main program

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -14,3 +14,12 @@ public:
 
 };
 
+int main(){
+    buku bukunya;
+    //bukunya.setJudul("Matematika");
+    //cout << bukunya.getJudul() << endl;
+
+    //Lebih simpelnya:
+    cout << bukunya.setJudul("Dosa Besar Soeharto").getJudul();     //Chain Function Calls
+    return 0;
+}
