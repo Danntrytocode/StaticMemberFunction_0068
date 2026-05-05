307466bc28ac501f248d6b71e085637aed88125e
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-05 17:50:57 +0700

    add class and nim sebagai static member

--- staticmember.cpp
+++ staticmember.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa {   //make a class
+
+public:
+    static int nim; //deklarasi variabel static "nim"
+    int id;
+    string nama;
+    
+}
