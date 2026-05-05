ad5eeef7414be7fdc3f8058ee904b2288b4ec457
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-05 17:54:46 +0700

    mendeklarasikan 2 prosedur, add constructor ber parameter

--- staticmember.cpp
+++ staticmember.cpp
@@ -7,5 +7,13 @@ public:
     static int nim; //deklarasi variabel static "nim"
     int id;
     string nama;
-    
+
+    //deklarasi prosedur setID dan printAll
+    void setID();
+    void printAll();
+
+    //making constructor Mahasiswa dgn parameter pnama
+    mahasiswa(string pnama) :nama(pnama){
+        setID();
+    }
 }
