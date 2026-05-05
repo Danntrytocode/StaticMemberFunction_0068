278434de687ca55e29d6a5c004832c0a0bba1149
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-05 17:45:09 +0700

    add program utama

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -13,3 +13,8 @@ mahasiswa::mahasiswa(int nim, string nama){
     cout << "NIM : " << nim << endl;
     cout << "Nama : " << nama << endl;
 }
+
+int main(){
+    mahasiswa msh(221, "Cumi Hitam Pak Kris");
+    return 0;
+}
